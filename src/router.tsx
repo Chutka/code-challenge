4e7903790b0619bf9696cfc97efa51aca15d9dc5
Chutka <chutkanub@mail.ru>
2023-10-15 00:18:04 +0400

    Disable main page

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from './components';
-import { Main, Challenge } from './pages';
+import { Challenge } from './pages';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
     children: [
+      // {
+      //   path: '',
+      //   element: <Main />
+      // },
       {
         path: '',
-        element: <Main />
-      },
-      {
-        path: '/:id',
         element: <Challenge />
       }
     ]
